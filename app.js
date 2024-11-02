9f47f0484846ab5ec8e7da0c7dd3affe33a6203e
go-nakao <79854138+go-nakao@users.noreply.github.com>
2024-11-02 17:39:23 +0900

    新しいページ追加

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//ページを追加
+var helloRouter = require('./routes/hello');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
